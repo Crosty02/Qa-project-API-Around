0cb463393bd873e8503d406ec6b0e53fe2a35193
Crosty02 <crystal.puentes31@gmail.com>
2025-01-28 12:12:50 -0500

    API Around pruebas

---
+++ find_element_exercise1.py
@@ -0,0 +1,12 @@
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+
+driver.get("https://around-v1.nm.tripleten-services.com/signin?lng=es")
+
+# Buscar el título
+driver.find_element(By.CSS_SELECTOR, ".auth-form__title")
+
+# Cerrar el navegador
+driver.quit()
